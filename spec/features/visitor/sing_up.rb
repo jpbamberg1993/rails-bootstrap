dd90e0e9e37b607b39452a2c6c27ddb0e9614531
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-11-09 22:53:13 -0500

    Test sign up.

---
+++ spec/features/visitor/sing_up.rb
@@ -0,0 +1,20 @@
+# Feature: Sign Up
+# 	As a visitor
+# 	I want to sign up
+# 	So I can view protected sections of the site
+feature 'Sign up' do
+	
+	# Scenario: Visitor Signs Up With Valid
+	# 	Given I am a visitor
+	# 	When I sign up with valid data 
+	# 	Then I should see a valid sign up message
+	scenario 'User Signs Up With Valid Data' do
+		visit new_user_registration_path
+		fill_in 'Name', :with => 'Test User'
+		fill_in 'Email', :with => 'user@example.com'
+		fill_in 'Password', :with => 'changeme'
+		fill_in 'Password Confirmation', :with => 'changeme'
+		click_button 'Sign Up'
+		expect(page).to have_content 'Welcome! You have signed up successfully.'
+	end
+end
