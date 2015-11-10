6d3500bf4cbb97d69439b122dd722f232434d241
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-11-10 18:35:27 -0500

    Test user email valid.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec generated test
+# RSpec.describe User, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe User do
+	
+	it "should be valid" do
+		user = User.new(email: 'user@example.com')
+		expect(user).to be_valid
+	end
+
 end
