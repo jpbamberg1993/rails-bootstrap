c7cb3695c8bb47d2a4c7e45710a7a10f7cbd97a7
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-11-10 20:08:37 -0500

    Test return using factory.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-# RSpec generated test
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
-
 describe User do
 	
+	before(:each) { @user = FactoryGirl.create(:user) }
+
 	it "should be valid" do
 		user = User.new(email: 'user@example.com')
 		expect(user).to be_valid
 	end
 
+	it 'all returns a list of users' do
+		expect(User.all.count).not_to eq 0
+	end
 end
