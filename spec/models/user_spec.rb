94994871e8d07baf5f8e09f061caeeac71da9236
jpbamberg1993@gmail.com <jpbamberg1993@gmail.com>
2015-11-10 20:19:33 -0500

    Test validations with factory.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,15 @@ describe User do
 	
 	before(:each) { @user = FactoryGirl.create(:user) }
 
-	it "should be valid" do
-		user = User.new(email: 'user@example.com')
+	it 'should be valid' do
+		user = FactoryGirl.build(:user)
 		expect(user).to be_valid
 	end
+
+	it "should be invalid with a blank email address" do
+    user = FactoryGirl.build(:user, email: nil)
+    expect(user).to_not be_valid
+  end
 
 	it 'all returns a list of users' do
 		expect(User.all.count).not_to eq 0
